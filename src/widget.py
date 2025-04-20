ad7ad3c1c116fe0195eb14ff1f3683cf03b1330b
Andrew Serebrennikov <andrey_s_009941@mail.ru>
2025-04-21 02:12:50 +0800

    widget after black

--- src/widget.py
+++ src/widget.py
@@ -1,10 +1,10 @@
 def get_mask_account_card(card_number: str) -> str:
-        """Функция для маскировки номера карты"""
+    """Функция для маскировки номера карты"""
     alpha_part_of_mask_card = ""
     for char in card_number:
         if char.isalpha() or char == " ":
             alpha_part_of_mask_card += char
-       return f"{alpha_part_of_mask_card}{card_number[-16:-11]} {card_number[-12:-10]}** **** {card_number[-4:]}"
+    return f"{alpha_part_of_mask_card}{card_number[-16:-11]} {card_number[-12:-10]}** **** {card_number[-4:]}"
 
 
 def get_date(full_date: str) -> str:
