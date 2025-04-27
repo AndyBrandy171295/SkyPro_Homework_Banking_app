50d384730b3a959b38ccb97907bbb333bb6f245d
Andrew Serebrennikov <andrey_s_009941@mail.ru>
2025-04-28 00:24:24 +0800

    filter_by_state and sort_by_date after black

--- src/processing.py
+++ src/processing.py
@@ -7,7 +7,7 @@ def filter_by_state(list_of_dict: [list], state: str = "EXECUTED") -> list[dict]
             new_filtered_list.append(dict)
     return new_filtered_list
 
+
 def sort_by_date(list_of_dicts: [list], reverse: bool = True) -> list[dict]:
     """Функция, которая принимает список словарей и возвращает новый, отсортированный по дате"""
     return sorted(list_of_dicts, key=lambda x: x["date"], reverse=True)
-
