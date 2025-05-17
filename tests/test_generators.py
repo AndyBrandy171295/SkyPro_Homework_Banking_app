81b3e4102cc8ba2f26174323dad9362c578c3588
Andrew Serebrennikov <andrey_s_009941@mail.ru>
2025-05-17 22:00:25 +0800

    add info in readme

---
+++ tests/test_generators.py
@@ -0,0 +1,60 @@
+import pytest
+from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
+
+
+def test_filter_by_currency_usd(transactions):
+    generator_result = filter_by_currency(transactions, "USD")
+    assert next(generator_result) == {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702",
+    }
+    assert next(generator_result) == {
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188",
+    }
+
+
+def test_filter_by_currency_rub(transactions):
+    generator_result = filter_by_currency(transactions, "RUB")
+    assert next(generator_result) == {
+        "id": 873106923,
+        "state": "EXECUTED",
+        "date": "2019-03-23T01:09:46.296404",
+        "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 44812258784861134719",
+        "to": "Счет 74489636417521191160",
+    }
+    assert next(generator_result) == {
+        "id": 594226727,
+        "state": "CANCELED",
+        "date": "2018-09-12T21:27:25.241689",
+        "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод организации",
+        "from": "Visa Platinum 1246377376343588",
+        "to": "Счет 14211924144426031657",
+    }
+
+
+@pytest.mark.usefixtures("transactions")
+def test_transaction_descriptions(transactions):
+    generator = transaction_descriptions(transactions)
+    assert next(generator) == "Перевод организации"
+    assert next(generator) == "Перевод со счета на счет"
+    assert next(generator) == "Перевод со счета на счет"
+
+
+def test_card_number_generator():
+    generator_result = card_number_generator(111, 222)
+    assert next(generator_result) == "0000 0000 0000 0111"
+    assert next(generator_result) == "0000 0000 0000 0112"
