dd1ee8590e49cca96a2a07f57b8d32b2b10adc7f
Andrew Serebrennikov <andrey_s_009941@mail.ru>
2025-06-03 02:09:09 +0800

    add info in readme

--- src/external_api.py
+++ src/external_api.py
@@ -48,20 +48,16 @@ def sum_transactions(transaction: dict) -> float:
                     total_sum += float(data_eur["result"])
                 else:
                     print("Ошибка запроса к API:", response.status_code)
+            else:
+                another_currency = i["operationAmount"]["currency"]["code"]
+                response = requests.get(
+                    f'https://api.apilayer.com/exchangerates_data/convert?to={to}&from={another_currency}&amount={amount}',
+                    headers=headers
+                )
+                if response.status_code == 200:
+                    data_another_currency = response.json()
+                    total_sum += float(data_another_currency["result"])
+                else:
+                    print("Ошибка запроса к API:", response.status_code)
 
     return total_sum
-
-qwer = [
-        {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702",
-        }
-    ]
-
-
-print(sum_transactions(qwer))
