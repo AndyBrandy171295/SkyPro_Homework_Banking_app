9e3f2ab28ced2e4693c1bcace67614df71a93c49
Andrew Serebrennikov <andrey_s_009941@mail.ru>
2025-06-04 02:57:51 +0800

    add info in readme

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,32 @@
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s: %(message)s',
+                    filename='D:/PythonProjects/Banking_app/logs/masks.log',
+                    filemode='w',
+                    encoding='utf-8')
+
+mask_card_logger = logging.getLogger('get_mask_card_number')
+mask_acc_logger = logging.getLogger('get_mask_account')
+
 def get_mask_card_number(card_number: str) -> str:
     """Функция показывает маску карты"""
+    mask_card_logger.info('Запуск функции.')
     if len(card_number) == 16:
+        mask_card_logger.info('Создание маски карты.')
         mask_card = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
         return mask_card
+    mask_card_logger.critical('Неверный номер карты!')
     return 'Неверный номер карты'
 
 
 def get_mask_account(account_number: str) -> str:
     """Функция показывает маску счета"""
-    mask_account = f"**{account_number[-4:]}"
-    return mask_account
+    mask_card_logger.info('Запуск функции.')
+    if account_number.isdigit():
+        mask_acc_logger.info('Создание маски счёте.')
+        mask_account = f"**{account_number[-4:]}"
+        return mask_account
+    mask_acc_logger.critical('Неверный номер счёта!')
+    return 'Неверный номер счёта!'
--- src/utils.py
+++ src/utils.py
@@ -1,22 +1,26 @@
 import json
+import logging
 
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(acstime)s - %(name)s - %(levelname)s: %(message)s',
+                    filename='D:/PythonProjects/Banking_app/logs/utils.log',
+                    filemode='w',
+                    encoding='utf-8')
+
+utils_logger = logging.getLogger('json_operation')
 
 def json_operation(json_path: str) -> list:
     '''Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях'''
     try:
+        utils_logger.info('Открываю json-файл.')
         with open(json_path, encoding='utf-8') as j_file:
             data = json.load(j_file)
-            print("Файл успешно прочитан, данные:", data)
+            utils_logger.info(f"Файл успешно прочитан, данные: {data}")
         return data
     except json.JSONDecodeError:
-        print('Ошибка чтения файла!')
+        utils_logger.error('Ошибка чтения файла!')
         return []
     except FileNotFoundError:
-        print('Файл не найден!')
+        utils_logger.error('Файл не найден!')
         return []
-
-
-if __name__ == '__mane__':
-    result = json_operation('data/operations.json')
-    print(result)
-
